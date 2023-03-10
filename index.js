1721d4644f14167ff04d33e26a45b0ccfa32f882
JASON YOUNG <moorslf@gmail.com>
2023-03-10 05:45:36 -0500

    arraysLab Submission

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+
+function appendCat(name) {
+    const copyCats = [...cats, "Broom"];
+    return copyCats;
+};
+
+
+function prependCat(name) {
+    const copyCats2 = ["Arnold", ...cats];
+    return copyCats2;
+};
+
+
+function removeLastCat() {
+    return cats.slice(0, 2);
+};
+
+
+function removeFirstCat() {
+    return cats.slice(1);
+};
